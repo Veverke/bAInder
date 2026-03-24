8dc7e5021e53ed4462588b4eca35fcd5ce25721f
Avraham Y. K. <avreimale@gmail.com>
2026-03-25 00:28:48 +0200

    feature + minor fixes

--- src/sidepanel/controllers/search-controller.js
+++ src/sidepanel/controllers/search-controller.js
@@ -148,6 +148,7 @@ export function handleSearch(event) {
     if (_hasActiveFilters()) {
       _handleSearchDeferred('');
     } else {
+      _expandTocSection();
       _state.renderer?.clearHighlight();
       hideSearchResults();
     }
